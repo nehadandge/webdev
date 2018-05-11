477b19e7859e3589756ec23a91c4f194645db5c6
nehadandge <39192047+nehadandge@users.noreply.github.com>
2018-05-11 18:25:58 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["usr"]; ?><br>
+Your email address is: <?php echo $_POST["pwd"]; ?>
+
+</body>
+</html>
---
+++ submit.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "qwerty";
+$dbname = "project";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+$sql = "INSERT INTO student_login VALUES ($_POST["usr"],$_POST["pwd"])";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
